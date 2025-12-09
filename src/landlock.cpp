6709d0cd73d2d4f9da4790f4b0064b6be51845e9
Marek Küthe <m.k@mk16.de>
2025-12-09 19:37:45 +0000

    Fix build

--- src/landlock.cpp
+++ src/landlock.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (C) 2025 Marek Küthe <m.k@mk16.de>
+// SPDX-FileCopyrightText: Copyright (C) 2024-2025 Marek Küthe <m.k@mk16.de>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -9,23 +9,17 @@
     #include <linux/landlock.h>
 
 LandlockRuleset::LandlockRuleset(uint64_t handled_access_fr,
-                                 uint64_t handled_access_net,
-                                 uint64_t scoped) :
-    abi_version(LandlockRuleset::get_abi_version())
+                                 uint64_t handled_access_net)
 {
-    if (this->abi_version < 6)
+    if (LandlockRuleset::abi_version() < 6)
         throw std::runtime_error("Landlock is too old.");
 
     const struct landlock_ruleset_attr attr = {
         .handled_access_fs = handled_access_fr,
-        .handled_access_net = handled_access_net,
-        .scoped = scoped};
-
-    this->ruleset = ::landlock_create_ruleset(&attr, sizeof(attr), 0);
+        .handled_access_net = handled_access_net};
+    this->ruleset = landlock_create_ruleset(&attr, sizeof(attr), 0);
     if (this->ruleset == -1)
-        throw std::system_error(errno,
-                                std::generic_category(),
-                                "Failed to create landlock ruleset");
+        throw std::runtime_error("Failed to create landlock ruleset.");
 }
 
 void LandlockRuleset::add_path_beneath_rule(uint64_t allowed_access,
@@ -33,53 +27,22 @@ void LandlockRuleset::add_path_beneath_rule(uint64_t allowed_access,
 {
     const struct landlock_path_beneath_attr attr = {
         .allowed_access = allowed_access, .parent_fd = parent_fd};
-    if (::landlock_add_rule(this->ruleset,
-                            LANDLOCK_RULE_PATH_BENEATH,
-                            static_cast<const void *>(&attr),
-                            0) != 0)
-        throw std::system_error(
-            errno, std::generic_category(), "Failed to add path beneath rule");
+    if (landlock_add_rule(this->ruleset,
+                          LANDLOCK_RULE_PATH_BENEATH,
+                          static_cast<const void *>(&attr),
+                          0) != 0)
+        throw std::runtime_error("Failed to add rule.");
 }
 
-void LandlockRuleset::add_net_port_rule(uint64_t allowed_access,
-                                        uint64_t port) const
-{
-    const struct landlock_net_port_attr attr = {
-        .allowed_access = allowed_access, .port = port};
-    if (::landlock_add_rule(this->ruleset,
-                            LANDLOCK_RULE_NET_PORT,
-                            static_cast<const void *>(&attr),
-                            0) != 0)
-        throw std::system_error(
-            errno, std::generic_category(), "Failed to add net port rule");
-}
-
-    #ifdef HAVE_LANDLOCK_RESTRICT_SELF_LOG_NEW_EXEC_ON
-void LandlockRuleset::restrict_self(uint32_t flags) const
-{
-    uint32_t compatility_flags = flags;
-    if (abi_version < 7)
-        compatility_flags &= ~LANDLOCK_RESTRICT_SELF_LOG_NEW_EXEC_ON;
-
-    if (::landlock_restrict_self(this->ruleset, compatility_flags) != 0)
-        throw std::system_error(errno,
-                                std::generic_category(),
-                                "Failed to restrict self via landlock");
-}
-    #else
 void LandlockRuleset::restrict_self() const
 {
-    if (::landlock_restrict_self(this->ruleset, 0) != 0)
-        throw std::system_error(errno,
-                                std::generic_category(),
-                                "Failed to restrict self via landlock");
+    if (landlock_restrict_self(this->ruleset, 0) != 0)
+        throw std::runtime_error("Failed to restrict self via landlock.");
 }
-    #endif
 
-[[nodiscard]] int LandlockRuleset::get_abi_version() noexcept
+[[nodiscard]] int LandlockRuleset::abi_version() noexcept
 {
-    return ::landlock_create_ruleset(
-        nullptr, 0, LANDLOCK_CREATE_RULESET_VERSION);
+    return landlock_create_ruleset(nullptr, 0, LANDLOCK_CREATE_RULESET_VERSION);
 }
 
 #endif
--- src/landlock.hpp
+++ src/landlock.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (C) 2025 Marek Küthe <m.k@mk16.de>
+// SPDX-FileCopyrightText: Copyright (C) 2024-2025 Marek Küthe <m.k@mk16.de>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -7,8 +7,7 @@
 
 #ifdef HAVE_LANDLOCK
 
-    #include <system_error>
-    #include <cerrno>
+    #include <stdexcept>
     #include <cstdint>
     #include <linux/landlock.h>
 
@@ -24,7 +23,7 @@ inline int landlock_create_ruleset(const struct landlock_ruleset_attr * attr,
                                    uint32_t flags)
 {
     return static_cast<int>(
-        ::syscall(SYS_landlock_create_ruleset, attr, size, flags));
+        syscall(SYS_landlock_create_ruleset, attr, size, flags));
 }
         #endif
 
@@ -34,7 +33,7 @@ inline int landlock_add_rule(int ruleset_fd,
                              const void * rule_attr,
                              uint32_t flags)
 {
-    return static_cast<int>(::syscall(
+    return static_cast<int>(syscall(
         SYS_landlock_add_rule, ruleset_fd, rule_type, rule_attr, flags));
 }
         #endif
@@ -43,7 +42,7 @@ inline int landlock_add_rule(int ruleset_fd,
 inline int landlock_restrict_self(int ruleset_fd, uint32_t flags)
 {
     return static_cast<int>(
-        ::syscall(SYS_landlock_restrict_self, ruleset_fd, flags));
+        syscall(SYS_landlock_restrict_self, ruleset_fd, flags));
 }
         #endif
 
@@ -53,21 +52,14 @@ class LandlockRuleset
 {
     public:
         explicit LandlockRuleset(uint64_t handled_access_fr,
-                                 uint64_t handled_access_net,
-                                 uint64_t scoped);
+                                 uint64_t handled_access_net);
         void add_path_beneath_rule(uint64_t allowed_access,
                                    int32_t parent_fd) const;
-        void add_net_port_rule(uint64_t allowed_access, uint64_t port) const;
-    #ifdef HAVE_LANDLOCK_RESTRICT_SELF_LOG_NEW_EXEC_ON
-        void restrict_self(uint32_t flags) const;
-    #else
         void restrict_self() const;
-    #endif
 
-        [[nodiscard]] static int get_abi_version() noexcept;
+        [[nodiscard]] static int abi_version() noexcept;
 
     private:
-        int abi_version;
         int ruleset;
 };
 
